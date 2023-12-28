4b330442ba1966a4ef8f845f4e34fdf3bd3da51e
nixberg <310825+nixberg@users.noreply.github.com>
2023-12-28 15:35:15 +0100

    Update SHA1Tests.swift

--- Tests/SHA1Tests/SHA1Tests.swift
+++ Tests/SHA1Tests/SHA1Tests.swift
@@ -6,6 +6,14 @@ final class SHA1Tests: XCTestCase {
     let testVectors = try! PackageResources.sha1_blb.blobs().couples()
     
     func test() {
+        XCTAssert(SHA1.hash(contentsOf: []).elementsEqual([
+            0xda, 0x39, 0xa3, 0xee, 0x5e, 0x6b, 0x4b, 0x0d,
+            0x32, 0x55, 0xbf, 0xef, 0x95, 0x60, 0x18, 0x90,
+            0xaf, 0xd8, 0x07, 0x09,
+        ]))
+    }
+    
+    func testBlob() {
         for (message, expectedOutput) in testVectors {
             XCTAssert(SHA1.hash(contentsOf: message).elementsEqual(expectedOutput))
         }
